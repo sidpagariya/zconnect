1eadd2b3269483d77c0f1e6491990523bfaa7f41
Yunsoo Kim <yunsoo@umich.edu>
2020-09-19 22:27:21 -0400

    optimized scraper not to scrape previously scraped messages

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+import time, json, requests
 
 URL = "https://us04web.zoom.us/wc/join/93275589158"
 driver = webdriver.Chrome(executable_path="./chromedriver")
@@ -10,23 +10,38 @@ name.send_keys("ZConnect")
 name.send_keys(Keys.ENTER)
 waiting_for_login = input()
 
-chatbox = driver.find_element_by_class_name("chat-box__chat-textarea")
-chatbox.send_keys("test")
-chatbox.send_keys(Keys.ENTER)
-
-chat_data = []
+# ASSUME that same person doesn't send more than 
+last_index = 0
 while(True):
     time.sleep(1)
     try:
-        chat = driver.find_elements_by_class_name("chat-item__chat-info")[-1]
-        usernames = chat.find_elements_by_tag_name("span")
-        sender = usernames[0].text
-        receiver = usernames[1].text
-        private = True if chat.find_elements_by_class_name("chat-privately") else False
-        text = chat.find_elements_by_class_name("chat-item__chat-info-msg")[0].text
-        tup = (sender, receiver, private, text)
-        if len(chat_data) == 0 or tup != chat_data[-1]:
-            chat_data.append(tup)
-        print(chat_data)
+        chats = driver.find_elements_by_class_name("chat-item__chat-info")
+        while (len(chats) > last_index):
+            chat = chats[last_index]
+            usernames = chat.find_elements_by_tag_name("span")
+            sender = usernames[0].text
+            if sender == "Me":
+                last_index += 1
+                continue
+            receiver = usernames[1].text
+            is_private = True if chat.find_elements_by_class_name("chat-privately") else False
+            text = chat.find_elements_by_class_name("chat-item__chat-info-msg")[0].text
+            data = {
+                "id": last_index,
+                "from": sender,
+                "to": receiver,
+                "text": text,
+                "is_private": is_private,
+            }
+            data_json = json.dumps(data)
+            print(data_json)
+            last_index += 1
+            response = {"text": "test"} # requests.post(url="https://gcp.sidp.me/data", json=data_json)
+            if response != "":
+                response_data = json.loads(response)
+                message = response_data["text"]
+                chatbox = driver.find_element_by_class_name("chat-box__chat-textarea")
+                chatbox.send_keys(message)
+                chatbox.send_keys(Keys.ENTER)
     except Exception:
         continue
