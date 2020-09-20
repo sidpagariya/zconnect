1ecdc97fe1320b98fbcb64103676e61f0d1757b0
Yunsoo Kim <yunsoo@umich.edu>
2020-09-20 00:28:54 -0400

    linked scraper with the backend

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,13 +3,14 @@ from selenium.webdriver.common.keys import Keys
 import time, json, requests
 
 URL = "https://us04web.zoom.us/wc/join/93275589158"
-driver = webdriver.Chrome(executable_path="./chromedriver")
+driver = webdriver.Chrome(executable_path="./scraper/chromedriver")
 driver.get(URL)
 name = driver.find_element_by_id("inputname")
 name.send_keys("ZConnect")
 name.send_keys(Keys.ENTER)
 waiting_for_login = input()
 
+# TODO: exit cleanly
 # ASSUME that same person doesn't send more than 
 last_index = 0
 while(True):
@@ -36,12 +37,15 @@ while(True):
             data_json = json.dumps(data)
             print(data_json)
             last_index += 1
-            response = {"text": "test"} # requests.post(url="https://gcp.sidp.me/data", json=data_json)
-            if response != "":
-                response_data = json.loads(response)
+            response = requests.post(url="http://gcp.sidp.me/data",json=data_json)
+            
+            if response.status_code == 200 and len(response.text) > 0:
+                response_data = response.json()
                 message = response_data["message"]
+                print(message)
                 chatbox = driver.find_element_by_class_name("chat-box__chat-textarea")
                 chatbox.send_keys(message)
                 chatbox.send_keys(Keys.ENTER)
     except Exception:
+        
         continue
