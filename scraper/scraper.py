0e4f3e05270e8615358b3cff22e5577972b7838a
Yunsoo Kim <yunsoo@umich.edu>
2020-09-20 03:17:59 -0400

    changed no response handling

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -10,7 +10,6 @@ name.send_keys("ZConnect")
 name.send_keys(Keys.ENTER)
 waiting_for_login = input()
 
-# TODO: exit cleanly
 # ASSUME that same person doesn't send more than 
 last_index = 0
 while(True):
@@ -27,6 +26,14 @@ while(True):
             receiver = usernames[1].text
             is_private = True if chat.find_elements_by_class_name("chat-privately") else False
             text = chat.find_elements_by_class_name("chat-item__chat-info-msg")[0].text
+            # if text == "/stopbot":
+            #     print("exiting")
+            #     leave_button = driver.find_elements_by_class_name("footer__leave-btn-container")[0].find_elements_by_tag_name("button")[0]
+            #     leave_button.click()
+            #     leave_meeting_button = driver.find_elements_by_class_name("leave-meeting-options__inner")[0].find_elements_by_tag_name("button")[0]
+            #     leave_meeting_button.click()
+            #     time.sleep(2)
+            #     driver.quit()
             data = {
                 "id": last_index,
                 "from": sender,
@@ -37,15 +44,17 @@ while(True):
             data_json = json.dumps(data)
             print(data_json)
             last_index += 1
+
             response = requests.post(url="http://gcp.sidp.me/data",json=data_json)
             
-            if response.status_code == 200 and len(response.text) > 0:
+            if response.status_code == 200:
                 response_data = response.json()
                 message = response_data["message"]
                 print(message)
-                chatbox = driver.find_element_by_class_name("chat-box__chat-textarea")
-                chatbox.send_keys(message)
-                chatbox.send_keys(Keys.ENTER)
-    except Exception:
-        
+                if message != "":
+                    chatbox = driver.find_element_by_class_name("chat-box__chat-textarea")
+                    chatbox.send_keys(message)
+                    chatbox.send_keys(Keys.ENTER)
+    except Exception as e:
+        print(type(e))
         continue
